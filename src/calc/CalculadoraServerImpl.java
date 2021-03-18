79dce709870909ba3dbf4d95777512d2d8451cf0
PauloPSantana <prog.santana@gmail.com>
2021-03-18 13:36:03 -0300

    Projeto criado

---
+++ src/calc/CalculadoraServerImpl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc;
+
+import java.util.Date;
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "calc.CalculadoraServer")
+public class CalculadoraServerImpl implements CalculadoraServer {
+
+  public float soma(float num1, float num2) {
+    return num1 + num2;
+  }
+
+  public float subtracao(float num1, float num2) {
+    return num1 - num2;
+  }
+
+  public float multiplicacao(float num1, float num2) {
+    return num1 * num2;
+  }
+
+  public float divisao(float num1, float num2) {
+    return num1 / num2;
+  }
+
+}
